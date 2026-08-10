ab5705fca675e75f8f9898415c86e0a978038111
zenjiro <18967498922@163.com>
2026-08-11 01:26:50 +0800

    feat: 策略卡片样式 + PolicyView 视图（3 分组 × ItemsControl + 玻璃卡片 + iOS 开关）

---
+++ wpf/Views/PolicyView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CaelusApp.WpfHost.Views
+{
+    public partial class PolicyView : UserControl
+    {
+        public PolicyView()
+        {
+            InitializeComponent();
+        }
+    }
+}
